4b286c240dfb5a262195af47dfdb3d8b53de6129
mdelahais <mdelahais1@gmail.com>
2017-09-12 21:34:53 +0200

    Add files via upload

---
+++ integration-web/js/app.js
@@ -0,0 +1,6 @@
+function popup() {
+    w2popup.open({
+        title: 'Popup Title',
+        body: '<div class="w2ui-centered">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit.</div>'
+    });
+}
